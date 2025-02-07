ef316f544b556196c1d2ba4f4ea3bdd135db576d
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-07 18:56:53 +0400

    still make some free places between codes to make code more readbale

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -3,16 +3,19 @@ import "./rate.css"
 
 export default function Rate({setSubmit,setRate,rate}) {
     const buttons=[1,2,3,4,5]
+
     return(
         <div className="rateDiv">
             <p>How did we do?</p>
             <p>Please let us know how we did with your support request.
                 All feedback is appreciated to help us improve our offering!</p>
+
             <div className="mapBtn">
             {buttons.map((item)=>
                 <button key={item} onClick={()=>setRate(item)}
                 style={{backgroundColor: rate === item?'orange':""}} >{item}</button>)}
             </div>
+            
             <button onClick={()=> rate && setSubmit(true)}>Submit</button>          
         </div>
     )
